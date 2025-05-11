6c5fd69cddb445f72eb7854ba4a7bcad10089fbd
JulianVal223 <julianvalencai84@gmail.com>
2025-05-11 01:35:02 -0500

    Cree la tabla

--- src/edu_pad/static/dataweb.py
+++ src/edu_pad/static/dataweb.py
@@ -3,14 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-
-
-
-
-
 class DataWeb:
     def __init__(self):
-        self.url = "https://finance.yahoo.com/quote/GC%3DF/history/"
+        self.url = "https://es.finance.yahoo.com/quote/GC%3DF/history/"
 
     def obtener_datos(self):
         try:
@@ -21,14 +16,13 @@ class DataWeb:
             respuesta = requests.get(self.url,headers=headers)
             if respuesta.status_code != 200:
                 print("La URL saco error, no respondio o no existe")
-            print(respuesta.text)
+            #print(respuesta.text)
+            soup = BeautifulSoup(respuesta.text,'html.parser')
+            tabla = soup.select_one('div[data-testid ="history-table"]table')
+            print(tabla)
         
         except Exception as err:
             print("error en la funcion obtener datos")
-
-
-
-
 
 
 dw = DataWeb()
