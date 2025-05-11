f02bbaa69cbbfe4c41c77f53240ada36d7efbf39
JulianVal223 <julianvalencai84@gmail.com>
2025-05-11 00:46:32 -0500

    clase dataweb

--- setup.py
+++ setup.py
@@ -15,3 +15,4 @@ setup(
         "beautifulsoup4"
     ]
 )
+
---
+++ src/edu_pad/static/dataweb.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+
+
+
+
+
+class DataWeb:
+    def __init__(self):
+        self.url = "https://finance.yahoo.com/quote/GC%3DF/history/"
+
+    def obtener_datos(self):
+        try:
+            # url cabeceras
+            headers = {
+                'User-Agent': 'Mozilla/5.0'
+            }
+            respuesta = requests.get(self.url,headers=headers)
+            if respuesta.status_code != 200:
+                print("La URL saco error, no respondio o no existe")
+            print(respuesta.text)
+        
+        except Exception as err:
+            print("error en la funcion obtener datos")
+
+
+
+
+
+
+dw = DataWeb()
+dw.obtener_datos()
+#print(dw.url)
+
+
